9275b18e1e39968434237550aa6635ef080fa20d
Nikolai Makarevich <nikoladze529@mail.ru>
2021-09-14 21:45:07 +0300

    feat: solve vigenere-cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,12 +20,65 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direction = true) {
+    this.direction = direction;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!')
+    }
+
+    message = message.toLowerCase();
+
+    if(key.length < message.length) {
+      key = key.repeat(message.length / key.length + 1).slice(0, message.length)
+    }
+
+    for (let i = 0; i < message.length; i++) {
+      if (message[i].charCodeAt() > 123 || message[i].charCodeAt() < 97) {
+        key = key.slice(0, i) + ' ' + key.slice(i)
+      }
+    }
+    
+    key = key.slice(0, message.length).toLowerCase()
+    
+    let result = message.split('').map((i, index) => {
+      let start = 97;
+      let shift = key.charCodeAt(index) - start;
+      let code = start + (i.charCodeAt() - start + shift) % 26;
+      console.log(shift)
+      return shift > 26 || shift < 0 ? i : String.fromCharCode(code).toUpperCase();
+    })
+
+    return this.direction ? result.join('') : result.reverse().join('')
+  }
+  decrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!')
+    }
+    
+    message = message.toLowerCase();
+
+    if(key.length < message.length) {
+      key = key.repeat(message.length / key.length + 1).slice(0, message.length)
+    }
+
+    for (let i = 0; i < message.length; i++) {
+      if (message[i].charCodeAt() > 123 || message[i].charCodeAt() < 97) {
+        key = key.slice(0, i) + ' ' + key.slice(i)
+      }
+    }
+    
+    key = key.slice(0, message.length).toLowerCase()
+    
+    let result = message.split('').map((i, index) => {
+      let start = 97;
+      let shift = key.charCodeAt(index) - start;
+      let code = start + (i.charCodeAt() - start - shift + 26) % 26;
+      console.log(shift)
+      return shift > 26 || shift < 0 ? i : String.fromCharCode(code).toUpperCase();
+    })
+
+    return this.direction ? result.join('') : result.reverse().join('')
   }
 }
