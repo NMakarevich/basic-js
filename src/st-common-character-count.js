ae5c293331e956790fadc4221b73c55471ddda16
Nikolai Makarevich <nikoladze529@mail.ru>
2021-09-14 15:54:32 +0300

    feat: solve any tasks

--- src/st-common-character-count.js
+++ src/st-common-character-count.js
@@ -11,7 +11,27 @@ import { NotImplementedError } from '../extensions/index.js';
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-export default function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getCommonCharacterCount(s1, s2) {
+  let count = 0;
+
+  let args = Array.from(arguments).map(arg => {
+    return Array.from(new Set(arg.split('')))
+    .map(i => {
+      let count = 0
+      for (let j = 0; j < arg.length; j++) {
+        if(arg[j] == i) count++
+      }
+      return [i, count]
+    })
+  })
+  
+  for (let i = 0; i < args[0].length; i++) {
+    for (let j = 0; j < args[1].length; j++){
+      if (args[1][j][0] == args[0][i][0]) {
+        count += Math.min(args[0][i][1], args[1][j][1])
+      }
+    }
+  }
+
+  return count
 }
--- src/st-sort-by-height.js
+++ src/st-sort-by-height.js
@@ -11,7 +11,8 @@ import { NotImplementedError } from '../extensions/index.js';
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-export default function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function sortByHeight(arr) {
+  let template = arr.map(i => i == -1 ? i : 0);
+  let sorted = arr.filter(i => i != -1).sort((a,b) => b - a);
+  return template.map(i => i == -1 ? i : sorted.pop())
 }
